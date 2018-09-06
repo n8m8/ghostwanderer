dc4f1cf2a9af661b19e48cb140e9dd9ac84e5e83
honglux <xuhlusa@gmail.com>
2018-09-06 10:53:03 -0400

    Honglu Xu Push Test
    A push test.

---
+++ Assets/Honglu_Xu_Push_Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Honglu_Xu_Push_Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
