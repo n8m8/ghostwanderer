f939d69d1a50422648a5fe9d5f1ac4aed3db7658
yxc775 <yxc775@case.edu>
2018-09-16 17:27:47 -0400

    adding basic enemy scripts, patrolling and chasing
    Test scene inside incase anyone want to know how it works

---
+++ Assets/ComponentsTest/Enemy/Scripts/chasingAI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class chasingAI : MonoBehaviour {
+	private GameObject playerObject;
+
+	NavMeshAgent agent;
+	Animator anim;
+
+	void Start() {
+		agent = GetComponent<NavMeshAgent> ();
+        playerObject = GameObject.Find("Player");
+    }
+	
+	void Update () {
+        agent.SetDestination(playerObject.transform.position);
+        //if very far away zombie speed = 0: idle animation
+        //if somewhat close zombie speed = walking speed: walking animation
+        //if near zombie speed = 8.0*walking speed:running animation
+        //transform.Translate(0, 0, _multiplier*speed * Time.deltaTime);
+    }
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject == playerObject) {
+            other.enabled = false;
+            this.enabled = false;
+		}
+	}
+
+
+
+	//private void OnSpeedChanged(float value) {
+	//	speed = baseSpeed * value;
+	//}
+}
---
+++ Assets/ComponentsTest/Enemy/Scripts/patrolingAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class patrolingAI : MonoBehaviour {
+    public Transform[] points;
+    private int desPoint = 0;
+    private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        GotoNextPoint();
+	}
+
+    void GotoNextPoint(){
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[desPoint].position;
+        desPoint = (desPoint + 1) % points.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!agent.pathPending && agent.remainingDistance < 8.0f)
+            GotoNextPoint();
+        
+
+		
+	}
+}
