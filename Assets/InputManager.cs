a5f85457aaa63dd7385e2a7d366b99e56126dc50
kylepham1606 <cpp31@case.edu>
2018-09-26 21:16:17 -0400

    Added a script called InputManager to the player
    3 buttons:
    - switch world
    - shoot (if we need, can change to something else later)
    - interact with objects (for dialogue and info reading)

---
+++ Assets/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    [SerializeField] private KeyCode switchWorldButton;
+    [SerializeField] private KeyCode shootButton;
+    [SerializeField] private KeyCode interactButton;
+
+    public System.Action OnSwitchButtonPressed;
+    public System.Action OnShootButtonPressed;
+    public System.Action OnInteractButtonPressed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(switchWorldButton) && OnSwitchButtonPressed != null) OnSwitchButtonPressed();
+        if (Input.GetKeyDown(shootButton) && OnShootButtonPressed != null) OnShootButtonPressed();
+        if (Input.GetKeyDown(interactButton) && OnInteractButtonPressed != null) OnInteractButtonPressed();
+    }
+}
