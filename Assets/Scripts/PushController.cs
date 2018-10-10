66a478d153421545e741c5b8bcc9ed28cbd5ad03
Jack Donnelly <deltadonnelly@gmail.com>
2018-10-09 21:40:08 -0400

    Add pushable objects
    Add a case that can be pushed

---
+++ Assets/Scripts/PushController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushController : MonoBehaviour {
+
+    [SerializeField] private GameObject PushableObject;
+    [SerializeField] private GameObject Protag;
+    [SerializeField] private GameObject Enemy;
+
+    public bool canMoveObject = false;
+    public bool enemyInRange = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canMoveObject = true;
+        }
+
+        if (Vector3.Distance(Enemy.transform.position, transform.position) < 4)
+        {
+            enemyInRange = true;
+        }
+
+        if (Input.GetKeyDown("f") && canMoveObject && enemyInRange)
+        {
+
+            PushableObject.GetComponent<Renderer>().enabled = false;
+            //Destroy(levelController.Vase);
+            //Destroy(levelController.VaseObject);
+
+        }
+        enemyInRange = false;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            canMoveObject = false;
+        }
+
+    }
+}
