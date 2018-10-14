343c9718a8e2bca09bd2586d4b70835a1ce02bf2
Jack Donnelly <deltadonnelly@gmail.com>
2018-10-14 17:02:36 -0400

    Add teleportation script

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject destination;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == player.tag)
+        {
+            player.transform.position = destination.transform.position;
+        }
+    }
+
+}
