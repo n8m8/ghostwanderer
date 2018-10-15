1f41e586304a2e3ad470a7584b34fde86d1b45b4
byrrice <32532726+byrrice@users.noreply.github.com>
2018-10-15 16:47:25 -0400

    Quick changes

---
+++ Assets/Scripts/PopUps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopUps : MonoBehaviour {
+
+	public TextMeshProUGUI textDisplay;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.name == "TestPlayer"){
+			textDisplay.text = "Press E to talk";
+		}
+	}
+
+	//Reset the checkflag 
+	void OnTriggerExit2D(Collider2D collider){
+		textDisplay.text = "";
+	}
+}
