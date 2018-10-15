41538188089f84ee4e76675812c855e5421d35c9
yxc775 <yxc775@case.edu>
2018-10-15 10:19:24 -0400

    edit enemy spawn, enemy will spawn after player gets ghost power

--- Assets/ComponentsTest/Enemy/Scripts/Spawn.cs
+++ Assets/ComponentsTest/Enemy/Scripts/Spawn.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour {
-    public GameObject enemy;
+    public GameObject[] enemy;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +18,9 @@ public class Spawn : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player")){
-            enemy.SetActive(true);
+            for (int i = 0; i < enemy.Length; i++){
+                enemy[i].SetActive(true);
+            }
         }
         
     }
