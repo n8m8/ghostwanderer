2a1dda2d63cf61220018df4331174aa44f67c605
byrrice <32532726+byrrice@users.noreply.github.com>
2018-10-16 02:44:43 -0400

    Only can access ghost dialogue while in ghost mode

---
+++ Assets/Scripts/GhostOnly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostOnly : MonoBehaviour {
+
+	private TestPlayerMove script;
+
+	// Use this for initialization
+	void Start () {
+		script = GameObject.Find("TestPlayer").GetComponent<TestPlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!script.isGhost){
+			this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			this.gameObject.GetComponent<Dialogue>().enabled = false;
+		}
+		else if (script.isGhost){
+			this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+			this.gameObject.GetComponent<Dialogue>().enabled = true;
+		}
+	}
+}
