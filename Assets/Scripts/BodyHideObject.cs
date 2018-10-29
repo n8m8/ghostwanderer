38736cafa1d5f194b6aa71ff6fed99b4aa975b3f
honglux <xuhlusa@gmail.com>
2018-10-29 15:09:17 -0400

    add sprite for the chest when contains body

---
+++ Assets/Scripts/BodyHideObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyHideObject : MonoBehaviour {
+
+    [SerializeField] private Sprite NormalSprite;
+    [SerializeField] private Sprite ContainBodySprite;
+
+    public bool ContainsBody { get; set; }
+
+    private bool last_body_flag;
+
+	// Use this for initialization
+	void Start () {
+        this.ContainsBody = false;
+        this.last_body_flag = ContainsBody;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        update_sprite();
+	}
+
+    private void update_sprite()
+    {
+        if(last_body_flag != ContainsBody)
+        {
+            if(ContainsBody)
+            {
+                GetComponent<SpriteRenderer>().sprite = ContainBodySprite;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = NormalSprite;
+            }
+            last_body_flag = ContainsBody;
+        }
+    }
+}
