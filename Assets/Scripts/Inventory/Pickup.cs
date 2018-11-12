4941a4fa14ea10247e927c40949d7e58a009c39b
Jack Donnelly <deltadonnelly@gmail.com>
2018-11-11 22:54:29 -0500

    Prevent searching when possession mechanic is possible

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -11,6 +11,7 @@ public class Pickup : MonoBehaviour {
     public TextMeshProUGUI inventoryDisplay;
 
     private bool itemZone = false;
+    private bool muteText = false;
     public static bool inProgress = false;
     // Use this for initialization
     void Start () {
@@ -28,7 +29,7 @@ public class Pickup : MonoBehaviour {
             StartCoroutine(addToInventory());
 
         }
-        else if (Input.GetKeyDown(KeyCode.R) && !itemZone && !inProgress)
+        else if (Input.GetKeyDown(KeyCode.R) && !itemZone && !inProgress && !muteText)
         {
             StartCoroutine(briefMessage("Nothing", 1));
         }
@@ -61,6 +62,10 @@ public class Pickup : MonoBehaviour {
             itemZone = true;
             coll = collider;
         }
+        if(collider.tag == "Possess")
+        {
+            muteText = true;
+        }
     }
 
     //Reset the checkflag 
@@ -69,6 +74,10 @@ public class Pickup : MonoBehaviour {
         if (collider.tag == "SpawnByState" || collider.tag == "EvidenceTrigger")
         {
             itemZone = false;
+        }
+        if (collider.tag == "Possess")
+        {
+            muteText = false;
         }
 
     }
