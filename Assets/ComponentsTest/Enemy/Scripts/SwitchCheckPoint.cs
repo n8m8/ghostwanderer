22189f9049e24adc36e76dca782971abd14b48c1
yxc775 <yxc775@case.edu>
2018-11-13 15:02:56 -0500

    adding checkpoings fix enemy bugs

---
+++ Assets/ComponentsTest/Enemy/Scripts/SwitchCheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCheckPoint : MonoBehaviour {
+    public Transform checkPoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player")){
+            collision.gameObject.GetComponent<CheckPointManager>().checkPoint = checkPoint;
+        }
+    }
+}
