d613bd9f3e201aa3bd2a6412f10db80f63c36194
yxc775 <yxc775@case.edu>
2018-11-13 14:14:11 -0500

    fix stun enemy bug

--- Assets/ComponentsTest/Enemy/Scripts/StunEnemy.cs
+++ Assets/ComponentsTest/Enemy/Scripts/StunEnemy.cs
@@ -7,6 +7,8 @@ public class StunEnemy : MonoBehaviour
     public GameObject[] targetEnemy; 
     public GameObject possessedObject;
     public bool isForever = false;
+    private PatrolingAI target1;
+    private StationAI target2;
     // Use this for initialization
     void Start()
     {
@@ -19,7 +21,7 @@ public class StunEnemy : MonoBehaviour
         for (int i = 0; i < targetEnemy.Length; i++)
         {
             if (targetEnemy[i] != null){
-                if(targetEnemy[i].GetComponent<PatrolingAI>().isStuned == false && possess.isPossessing && targetEnemy[i].GetComponent<PatrolingAI>().getDistance() < 5f)
+                if(checkStunSetting(targetEnemy[i]) == false && possess.isPossessing && targetEnemy[i].GetComponent<PatrolingAI>().getDistance() < 5f)
                 {
                     StartCoroutine(stunEnemy(targetEnemy[i]));
                 }
@@ -28,16 +30,47 @@ public class StunEnemy : MonoBehaviour
 
     }
 
+    bool checkStunSetting(GameObject targetEnemy){
+        PatrolingAI target = targetEnemy.GetComponent<PatrolingAI>();
+        StationAI target2 = targetEnemy.GetComponent<StationAI>();
+        if(target == null){
+            return target2.isStuned;
+        }
+        else{
+            return target.isStuned;
+        }
+    }
+
     IEnumerator stunEnemy(GameObject targetEnemy){
-        if (isForever)
+        PatrolingAI target = targetEnemy.GetComponent<PatrolingAI>();
+
+        if (target == null)
         {
-            targetEnemy.GetComponent<PatrolingAI>().isStuned = true;
+            if (isForever)
+            {
+
+                targetEnemy.GetComponent<StationAI>().isStuned = true;
+            }
+            else
+            {
+                targetEnemy.GetComponent<StationAI>().isStuned = true;
+                yield return new WaitForSeconds(10f);
+                targetEnemy.GetComponent<StationAI>().isStuned = false;
+            }
         }
         else
         {
-            targetEnemy.GetComponent<PatrolingAI>().isStuned = true;
-            yield return new WaitForSeconds(10f);
-            targetEnemy.GetComponent<PatrolingAI>().isStuned = false;
+            if (isForever)
+            {
+
+                targetEnemy.GetComponent<PatrolingAI>().isStuned = true;
+            }
+            else
+            {
+                targetEnemy.GetComponent<PatrolingAI>().isStuned = true;
+                yield return new WaitForSeconds(10f);
+                targetEnemy.GetComponent<PatrolingAI>().isStuned = false;
+            }
         }
     }
 }
