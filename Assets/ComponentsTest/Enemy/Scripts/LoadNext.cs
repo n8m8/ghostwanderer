c4f3a6443450d54d0954f1daf0f5ca4a419e75eb
yxc775 <yxc775@case.edu>
2018-11-13 15:36:26 -0500

    add save points add scene switche

---
+++ Assets/ComponentsTest/Enemy/Scripts/LoadNext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNext : MonoBehaviour {
+    public int SceneNumber;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene(SceneNumber);
+        }
+    }
+}
