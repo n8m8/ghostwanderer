3a78ed083054028dea995b934e4bacc760ea2613
jnovak98 <36611520+jnovak98@users.noreply.github.com>
2018-11-15 13:55:32 -0500

    Made cardboard box a working prefab and added it to scene

---
+++ Assets/Scripts/CardboardBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag == "Player" && Input.GetKeyDown("e"))
+			GetComponent<Animator>().SetTrigger("Open");
+	}
+
+}
