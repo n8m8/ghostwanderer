8e2b520e6dfe0df92a70e5f0af6ccf16a3a49e91
jnovak98 <36611520+jnovak98@users.noreply.github.com>
2018-11-20 15:11:26 -0500

    Made TV Only stay on for limited amount of time

--- Assets/ComponentsTest/Enemy/Scripts/InteractingObject.cs
+++ Assets/ComponentsTest/Enemy/Scripts/InteractingObject.cs
@@ -8,6 +8,10 @@ public class InteractingObject : MonoBehaviour
     public bool canMoveObject = false;
 	[SerializeField] private GameObject obj;
     public GameObject enemy;
+	[SerializeField] private float duration;
+
+	private float timeOn;
+	private bool tvOn;
 
     private StationAI enemyController;
     private PatrolingAIDistrac enemyController2;
@@ -17,31 +21,66 @@ public class InteractingObject : MonoBehaviour
     {
         enemyController = enemy.GetComponent<StationAI>();
         enemyController2 = enemy.GetComponent<PatrolingAIDistrac>();
+		timeOn = 0;
+		tvOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e") && canMoveObject)
-        {
-            if (enemyController != null)
-            {
-                enemyController.distracted = true;
-            }
-
-            if (enemyController2 != null)
-            {
-                enemyController2.distracted = true;
-            }
-            if (obj.GetComponent<AudioSource>() != null)
-                obj.GetComponent<AudioSource>().Play();
-            try
-            {
-                GetComponentInParent<Animator>().SetTrigger("Lure");
-            }
-            catch { Debug.Log("lure bug " + transform.name); }
-        }
+		if (Input.GetKeyDown ("e") && canMoveObject && !tvOn)
+			turnOn ();
+		if (tvOn){
+			timeOn += Time.deltaTime;
+			if (timeOn > duration)
+				turnOff ();
+		}
     }
+
+	void turnOn(){
+		if (enemyController != null)
+		{
+			enemyController.distracted = true;
+		}
+
+		if (enemyController2 != null)
+		{
+			enemyController2.distracted = true;
+		}
+		if (obj.GetComponent<AudioSource>() != null)
+			obj.GetComponent<AudioSource>().Play();
+		try
+		{
+			GetComponentInParent<Animator>().SetTrigger("Lure");
+		}
+		catch {
+			Debug.Log ("lure bug " + transform.name);
+		}
+		tvOn = true;
+	}
+
+	void turnOff(){
+		if (enemyController != null)
+		{
+			enemyController.distracted = false;
+		}
+
+		if (enemyController2 != null)
+		{
+			enemyController2.distracted = false;
+		}
+		if (obj.GetComponent<AudioSource>() != null)
+			obj.GetComponent<AudioSource>().Stop();
+		try
+		{
+			GetComponentInParent<Animator>().SetTrigger("Stop");
+		}
+		catch {
+			Debug.Log ("lure bug " + transform.name);
+		}
+		tvOn = false;
+		timeOn = 0;
+	}
 
     void OnTriggerStay2D(Collider2D other)
     {
