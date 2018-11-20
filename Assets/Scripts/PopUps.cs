9003150f7e672d91ee15dc02d7b76dc46f108d77
jnovak98 <36611520+jnovak98@users.noreply.github.com>
2018-11-20 14:56:56 -0500

    Made new button popups work on more stuff

--- Assets/Scripts/PopUps.cs
+++ Assets/Scripts/PopUps.cs
@@ -6,22 +6,46 @@ using TMPro;
 
 public class PopUps : MonoBehaviour {
 
-	public GameObject buttomImage;
+	[SerializeField] private GameObject buttomImage;
 	private TestPlayerMove playerScript;
-	[SerializeField] bool ghostOnly;
+	[SerializeField] private bool ghostOnly;
+	[SerializeField] private bool humanOnly;
+	[SerializeField] private bool repeatable;
+	[SerializeField] private Vector3 relativeLocation = new Vector3(0,1,0);
+	[SerializeField] private string actionKey = "e";
+
 	public bool hidden = false;
 	private GameObject popup;
+	private bool doneOnce;
 
 	void Start(){
 		playerScript = GameObject.Find("TestPlayer").GetComponent<TestPlayerMove>();
+		doneOnce = false;
+	}
+
+	void Update(){
+		if (popup != null) {
+			
+			//if action can only be done once, removes popup once its done
+			if (Input.GetKeyDown (actionKey)) {
+				if (!repeatable)
+					Destroy (popup);
+				doneOnce = true;
+			}
+		}
 	}
 	void OnTriggerEnter2D(Collider2D collider){
-		if(!ghostOnly || playerScript.isGhost)
-			popup = (GameObject) Instantiate(buttomImage, transform.position+ new Vector3(0,1f,0), transform.rotation);
+		if(collider.tag=="Player"){
+		//ghost mode prompts only appear once you unlock ghost mode
+			if (!actionKey.Equals ("q") || playerScript.ghostAvailable) { 
+				if ((!ghostOnly || playerScript.isGhost) && (!humanOnly || !playerScript.isGhost) && (repeatable || !doneOnce))
+					popup = (GameObject)Instantiate (buttomImage, transform.position + relativeLocation, new Quaternion ());
+			} 
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider){
-		 //buttomImage.GetComponent<RawImage>().enabled= false;
-		 Destroy(popup);
+		if(collider.tag=="Player")
+			Destroy(popup);
 	}
 }
