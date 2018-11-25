5fb85ac152dc1bfbc42ef2b18b73415b25e88046
jnovak98 <36611520+jnovak98@users.noreply.github.com>
2018-11-25 14:46:33 -0500

    Added Unavoidable Dialogue Script
    Exactly the same as normal dialogue except only triggers once and triggers automatically once player enters collider

---
+++ Assets/Scripts/UnavoidableDialogue.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UnavoidableDialogue : MonoBehaviour {
+
+	public TextMeshProUGUI textDisplay;
+	public string[] sentences;
+	private int index;
+	public float typingSpeed;
+	public GameObject continueButton;
+	public string name; 
+	public GameObject image;
+	private RawImage dialogueBox;
+	private bool checkflag = false;
+	//True if the player is in range to pick up and item 
+	private bool itemZone = false;
+	public bool inProgress = false;
+	private bool triggered = false;
+
+	void Start(){
+		continueButton.SetActive(false);
+		image.SetActive(false);
+		dialogueBox = GameObject.FindGameObjectWithTag("DialogueBox").GetComponent<RawImage>();
+		dialogueBox.enabled = false;
+	}
+
+	void Update(){
+
+		//start the dialogue
+		if (!triggered && checkflag){
+			triggered = true;
+			StartCoroutine(Type());
+			//GameObject.Find("TestPlayer").GetComponent<TestPlayerMove>().enabled = false;
+			GameObject.Find("TestPlayer").GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+			checkflag = false;
+			inProgress = true;
+			Pickup.inProgress = true;
+		}
+
+		//load the continue dialogue button
+		if (textDisplay.text == (name + sentences[index])){
+			continueButton.SetActive(true);
+		}
+
+		//call next sentence method upon completely loaded sentence and key press
+		if (Input.GetKeyDown(KeyCode.E) && textDisplay.text == (name + sentences[index]) && !checkflag){
+			NextSentence();
+		}
+
+		//skip the dialogue after keypress 
+		if (Input.GetKeyDown(KeyCode.Space) && textDisplay.text == (name + sentences[index]) && !checkflag){
+			resetDialogue();
+		}
+	}
+
+
+	//Load the text in one by one
+	IEnumerator Type(){
+		image.SetActive(true);
+		dialogueBox.enabled = true;
+		//check for animation type here and execute it maybe enums?? 
+		textDisplay.text += name;
+		foreach(char letter in sentences[index].ToCharArray()){
+			textDisplay.text += letter;
+			yield return new WaitForSeconds(typingSpeed);
+		}
+
+	}
+
+	//load the next sentence
+	public void NextSentence(){
+		if (index < sentences.Length - 1){
+			index++;
+			textDisplay.text = "";
+			StartCoroutine(Type());
+			continueButton.SetActive(false);
+		}
+		else{
+			resetDialogue();
+		}
+	}
+
+	//checkflag is true if player
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.name == "TestPlayer"){
+			checkflag = true;
+		}
+	}
+
+	//Reset the checkflag 
+	void OnTriggerExit2D(Collider2D collider){
+		if(collider.name == "TestPlayer")
+		{
+			checkflag = false;
+		}
+
+	}
+
+	//reset the dialogue box 
+	public void resetDialogue(){
+		image.SetActive(false);
+		dialogueBox.enabled = false;
+		textDisplay.text = "";
+		continueButton.SetActive(false);
+		index = 0;
+		GameObject.Find("TestPlayer").GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+		GameObject.Find("TestPlayer").GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+		//GameObject.Find("TestPlayer").GetComponent<TestPlayerMove>().enabled = true;
+		checkflag = true;
+		inProgress = false;
+		Pickup.inProgress = false;
+	}
+
+
+
+}
