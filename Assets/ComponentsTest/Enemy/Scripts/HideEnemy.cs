8618e5cd4bb9aff3ab737717b0366a6d93fe992f
yxc775 <yxc775@case.edu>
2018-11-27 13:25:41 -0500

    hide enemy when player not in the room

---
+++ Assets/ComponentsTest/Enemy/Scripts/HideEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideEnemy : MonoBehaviour {
+    public GameObject[] enemy;
+    private bool isIn;
+
+	// Use this for initialization
+	void Start () {
+        isIn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemy[0].activeSelf)
+        {
+            if (isIn)
+            {
+                for (int i = 0; i < enemy.Length; i++)
+                {
+                    enemy[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < enemy.Length; i++)
+                {
+                    enemy[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player")){
+            isIn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isIn = false;
+        }
+    }
+}
