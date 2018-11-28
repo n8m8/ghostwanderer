118d41ed037fece465ddee92fc76b2cba1553d8a
yxc775 <yxc775@case.edu>
2018-11-28 14:41:58 -0500

    evidence connecting

---
+++ Assets/EnemyAndInteraction/Scripts/EvidenceTracker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvidenceTracker : MonoBehaviour {
+    private EvidenceManager manager;
+    public GameObject evi1;
+    public GameObject evi2;
+    public GameObject evi3;
+    public GameObject evi4;
+    public GameObject evi5;
+    public GameObject evi6;
+    public GameObject evi7;
+    public GameObject evi8;
+
+	// Use this for initialization
+	void Start () {
+        if(GameObject.FindGameObjectWithTag("Evidences") == null){
+
+        }
+        else{
+            manager = GameObject.FindGameObjectWithTag("Evidences").gameObject.GetComponent<EvidenceManager>();
+            LoadEvidences();
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadEvidences(){
+        List<int> evidences = manager.evidences;
+
+
+    }
+
+    bool isInList(int code, List<int> list){
+        return list.IndexOf(code) != -1;
+    }
+}
