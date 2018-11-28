3427f973c06051f4d89fb1d29e2fa96ce48f74b2
yxc775 <yxc775@case.edu>
2018-11-28 15:27:48 -0500

    evidence collection finished

--- Assets/EnemyAndInteraction/Scripts/EvidenceTracker.cs
+++ Assets/EnemyAndInteraction/Scripts/EvidenceTracker.cs
@@ -15,25 +15,93 @@ public class EvidenceTracker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        if(GameObject.FindGameObjectWithTag("Evidences") == null){
-
-        }
-        else{
-            manager = GameObject.FindGameObjectWithTag("Evidences").gameObject.GetComponent<EvidenceManager>();
-            LoadEvidences();
-        }
-		
+        evi1.SetActive(false);
+        evi2.SetActive(false);
+        evi3.SetActive(false);
+        evi4.SetActive(false);
+        evi5.SetActive(false);
+        evi6.SetActive(false);
+        evi7.SetActive(false);
+        evi8.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(manager != null){
+            updateEvidences();
+        }
+        else{
+            if (GameObject.FindGameObjectWithTag("Evidences") == null)
+            {
+
+            }
+            else
+            {
+                manager = GameObject.FindGameObjectWithTag("Evidences").gameObject.GetComponent<EvidenceManager>();
+            }
+        }
 		
 	}
 
-    void LoadEvidences(){
+    void updateEvidences(){
         List<int> evidences = manager.evidences;
+        if(isInList(0,evidences)){
+            evi1.SetActive(true);
+        }
+        else{
+            evi1.SetActive(false);
+        }
 
+        if(isInList(1,evidences)){
+            evi2.SetActive(true);
+        }
+        else{
+            evi2.SetActive(false);
+        }
 
+        if (isInList(2, evidences))
+        {
+            evi3.SetActive(true);
+        }
+        else{
+            evi3.SetActive(false);
+        }
+        if (isInList(3, evidences))
+        {
+            evi4.SetActive(true);
+        }
+        else{
+            evi4.SetActive(false);
+        }
+        if (isInList(4, evidences))
+        {
+            evi5.SetActive(true);
+        }
+        else{
+            evi5.SetActive(false);
+        }
+
+        if (isInList(5, evidences))
+        {
+            evi6.SetActive(true);
+        }
+        else{
+            evi6.SetActive(false);
+        }
+        if (isInList(6, evidences))
+        {
+            evi7.SetActive(true);
+        }
+        else{
+            evi7.SetActive(false);
+        }
+        if (isInList(7, evidences))
+        {
+            evi8.SetActive(true);
+        }
+        else{
+            evi8.SetActive(false);
+        }
     }
 
     bool isInList(int code, List<int> list){
