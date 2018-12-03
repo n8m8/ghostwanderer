2a68bde17f392bf11b5910168345d68c24a7ac1d
byrrice <32532726+byrrice@users.noreply.github.com>
2018-12-02 21:47:34 -0500

    Tenative Evidence + UI fixes

--- Assets/EnemyAndInteraction/Scripts/EvidenceManager.cs
+++ Assets/EnemyAndInteraction/Scripts/EvidenceManager.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EvidenceManager : MonoBehaviour {
     public List<int> evidences;
+    private TextMeshProUGUI textDisplay;
+    private Scene scene;
+    private int firstlevelcount = 0;
+
 	// Use this for initialization
     void Start () {
+        textDisplay = GameObject.FindGameObjectWithTag("EvidenceDisplay").GetComponent<TextMeshProUGUI>();
+        scene = SceneManager.GetActiveScene();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +25,24 @@ public class EvidenceManager : MonoBehaviour {
         {
             evidences.Add(evidenceCode);
         }
+        if (scene.name == "FirstLevel"){
+            updateEvidence(6);
+        }
+        else if (scene.name == "SecondLevel"){
+            updateEvidence(4);
+        }
+    }
+
+    public void updateEvidence(int numEvidence){
+        Debug.Log("lmaozedeong");
+        if (scene.name == "FirstLevel"){
+            textDisplay.text = "Evidence Collected: " + evidences.Count.ToString() + "/" + numEvidence.ToString();
+            firstlevelcount++;
+        }
+        else if (scene.name == "SecondLevel"){
+            int count = evidences.Count - firstlevelcount;
+            textDisplay.text = "Evidence Collected: " + count.ToString() + "/" + numEvidence.ToString();
+        }
+        
     }
 }
