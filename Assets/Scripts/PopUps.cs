3d73e8623dcf641f9230ef97ded94024e032105b
byrrice <32532726+byrrice@users.noreply.github.com>
2018-12-03 14:16:04 -0500

    attempt to get e pres working

--- Assets/Scripts/PopUps.cs
+++ Assets/Scripts/PopUps.cs
@@ -48,4 +48,6 @@ public class PopUps : MonoBehaviour {
 		if(collider.tag=="Player")
 			Destroy(popup);
 	}
+
+
 }
---
+++ Assets/ePress.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ePress : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			StartCoroutine(Type());
+			GetComponent<Animator>().SetBool("ePress", false);
+		}
+	}
+
+		//Load the text in one by one
+    IEnumerator Type(){
+		GetComponent<Animator>().SetBool("ePress", true);
+		yield return new WaitForSeconds(2);
+
+	}
+}
